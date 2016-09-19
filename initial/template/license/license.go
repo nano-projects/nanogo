d60c54ee13ba2be8599077492f9c08b9e6604725
yanghe <comicme_yanghe@icloud.com>
2016-09-19 14:16:07 +0800

    #45 update:   initial/tmplate/license/license.go

--- initial/template/license/license.go
+++ initial/template/license/license.go
@@ -89,7 +89,6 @@ func Class() string {
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-`
+ */`
 
 }
